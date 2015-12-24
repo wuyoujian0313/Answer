c8ddc86fc78a0d0d8619f4e522e9f940da42d415
wuyoujian0313 <wuyoujian0313@163.com>
2015-12-24 18:36:44 +0800

    继续增加视频录音功能
    继续增加视频录音功能

--- Answer/AnswerCircleVC.m
+++ Answer/AnswerCircleVC.m
@@ -9,10 +9,17 @@
 #import "AnswerCircleVC.h"
 #import "QuestionsView.h"
 #import "QuestionTableViewCell.h"
+#import <CoreMedia/CoreMedia.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface AnswerCircleVC ()<QuestionTableViewCellDelegate>
-@property (nonatomic, strong) QuestionsView *questionView;
-
+@interface AnswerCircleVC ()<QuestionTableViewCellDelegate,AVAudioPlayerDelegate>
+@property (nonatomic, strong) QuestionsView             *questionView;
+@property(nonatomic,strong)AVAudioPlayer                *audioPlayer;
+@property(nonatomic,strong)NSURL                        *recordedFile;
+@property(nonatomic,copy)NSString                       *videoPathString;
+@property(nonatomic,strong)MPMoviePlayerViewController  *moviePlayer;
 @end
 
 @implementation AnswerCircleVC
@@ -25,10 +32,59 @@
     [self layoutQuestionView];
 }
 
+-(void)playVideo:(UIButton*)sender {
+    
+    NSString  *src = _videoPathString;
+    if (src != nil && [src length] > 0) {
+        self.moviePlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL fileURLWithPath:src]];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(movieFinishedCallback:) name:MPMoviePlayerPlaybackDidFinishNotification object:_moviePlayer.moviePlayer];
+        [_moviePlayer.moviePlayer setControlStyle: MPMovieControlStyleFullscreen];
+        [_moviePlayer.moviePlayer play];
+        
+        [self presentMoviePlayerViewControllerAnimated:_moviePlayer];
+    }
+}
+
+-(void)movieFinishedCallback:(NSNotification *)notify {
+    
+    MPMoviePlayerController *vc = [notify object];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:vc];
+    
+    _moviePlayer = nil;
+}
+
+-(void)playReordFile {
+    
+    //#if TARGET_IPHONE_SIMULATOR
+    //#elif TARGET_OS_IPHONE
+    // 播放
+    NSError *playerError;
+    self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:_recordedFile error:&playerError];
+    if (_audioPlayer) {
+        _audioPlayer.delegate = self;
+        [_audioPlayer prepareToPlay];
+        [_audioPlayer play];
+    } else {
+        NSLog(@"ERror creating player: %@", [playerError description]);
+    }
+    //#endif
+    
+}
+
 - (void)layoutQuestionView {
     self.questionView = [[QuestionsView alloc] initWithFrame:CGRectMake(0, navigationBarHeight, screenWidth, screenHeight - navigationBarHeight - 49) delegate:self];
     
     [self.view addSubview:_questionView];
+}
+
+#pragma mark - AVAudioPlayerDelegate
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+    [_audioPlayer stop];
+}
+
+- (void)audioPlayerDecodeErrorDidOccur:(AVAudioPlayer *)player error:(NSError *)error {
+    [_audioPlayer stop];
 }
 
 
@@ -36,6 +92,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 
 #pragma mark - QuestionTableViewCellDelegate
--- Answer/QuestionDetailVC.m
+++ Answer/QuestionDetailVC.m
@@ -7,9 +7,16 @@
 //
 
 #import "QuestionDetailVC.h"
+#import <CoreMedia/CoreMedia.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface QuestionDetailVC ()
-
+@interface QuestionDetailVC ()<AVAudioPlayerDelegate>
+@property(nonatomic,strong)AVAudioPlayer                *audioPlayer;
+@property(nonatomic,strong)NSURL                        *recordedFile;
+@property(nonatomic,copy)NSString                       *videoPathString;
+@property(nonatomic,strong)MPMoviePlayerViewController  *moviePlayer;
 @end
 
 @implementation QuestionDetailVC
@@ -17,6 +24,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+-(void)playVideo:(UIButton*)sender {
+    
+    NSString  *src = _videoPathString;
+    if (src != nil && [src length] > 0) {
+        self.moviePlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL fileURLWithPath:src]];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(movieFinishedCallback:) name:MPMoviePlayerPlaybackDidFinishNotification object:_moviePlayer.moviePlayer];
+        [_moviePlayer.moviePlayer setControlStyle: MPMovieControlStyleFullscreen];
+        [_moviePlayer.moviePlayer play];
+        
+        [self presentMoviePlayerViewControllerAnimated:_moviePlayer];
+    }
+}
+
+-(void)movieFinishedCallback:(NSNotification *)notify {
+    
+    MPMoviePlayerController *vc = [notify object];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:vc];
+    
+    _moviePlayer = nil;
+}
+
+-(void)playReordFile {
+    
+    //#if TARGET_IPHONE_SIMULATOR
+    //#elif TARGET_OS_IPHONE
+    // 播放
+    NSError *playerError;
+    self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:_recordedFile error:&playerError];
+    if (_audioPlayer) {
+        _audioPlayer.delegate = self;
+        [_audioPlayer prepareToPlay];
+        [_audioPlayer play];
+    } else {
+        NSLog(@"ERror creating player: %@", [playerError description]);
+    }
+    //#endif
+    
+}
+
+#pragma mark - AVAudioPlayerDelegate
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+    [_audioPlayer stop];
+}
+
+- (void)audioPlayerDecodeErrorDidOccur:(AVAudioPlayer *)player error:(NSError *)error {
+    [_audioPlayer stop];
 }
 
 - (void)didReceiveMemoryWarning {
