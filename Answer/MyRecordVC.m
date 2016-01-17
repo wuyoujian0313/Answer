c4570efd89dcf1e4f5d0041660f6e7dcc23a94d7
wuyoujian0313 <wuyoujian0313@163.com>
2016-01-17 15:24:43 +0800

    增加交易记录布局
    增加交易记录布局

--- Answer/MyRecordVC.m
+++ Answer/MyRecordVC.m
@@ -7,6 +7,12 @@
 //
 
 #import "MyRecordVC.h"
+#import "NetworkTask.h"
+
+@interface MyRecordVC ()<UITableViewDataSource,UITableViewDelegate,NetworkTaskDelegate>
+@property(nonatomic,strong)UITableView          *recordTableView;
+@property(nonatomic,strong)NSArray              *recordList;
+@end
 
 @implementation MyRecordVC
 
@@ -14,6 +20,80 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self setNavTitle:@"交易记录"];
+    
+    [self layoutRecordTableView];
+}
+
+- (void)layoutRecordTableView {
+    UITableView * tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, navigationBarHeight, self.view.frame.size.width, self.view.frame.size.height - navigationBarHeight) style:UITableViewStylePlain];
+    [self setRecordTableView:tableView];
+    [tableView setDelegate:self];
+    [tableView setDataSource:self];
+    [tableView setBackgroundColor:[UIColor clearColor]];
+    [tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    [self.view addSubview:tableView];
+    
+    [self setTableViewHeaderView:12];
+    [self setTableViewFooterView:0];
+}
+
+-(void)setTableViewHeaderView:(NSInteger)height {
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, _recordTableView.frame.size.width, height)];
+    view.backgroundColor = [UIColor clearColor];
+    [_recordTableView setTableHeaderView:view];
+}
+
+-(void)setTableViewFooterView:(NSInteger)height {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, _recordTableView.frame.size.width, height)];
+    view.backgroundColor = [UIColor clearColor];
+    [_recordTableView setTableFooterView:view];
+}
+
+#pragma mark - NetworkTaskDelegate
+-(void)netResultSuccessBack:(NetResultBase *)result forInfo:(id)customInfo {
+    [SVProgressHUD dismiss];
+}
+
+-(void)netResultFailBack:(NSString *)errorDesc errorCode:(NSInteger)errorCode forInfo:(id)customInfo {
+    [SVProgressHUD dismiss];
+    [FadePromptView showPromptStatus:errorDesc duration:1.0 finishBlock:^{
+        //
+    }];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [_recordList count];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *cellIdentifier = @"recordTableCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
+        
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        LineView *line = [[LineView alloc] initWithFrame:CGRectMake(0, 50-kLineHeight1px, tableView.frame.size.width, kLineHeight1px)];
+        [cell.contentView addSubview:line];
+    }
+
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 50;
 }
 
 @end
