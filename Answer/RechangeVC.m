edf3aff3309399d6343d4db99d3659acf7816d25
wuyoujian0313 <wuyoujian0313@163.com>
2016-02-21 17:54:36 +0800

    支持支付宝支付sdk
    支持支付宝支付sdk

--- Answer/RechangeVC.m
+++ Answer/RechangeVC.m
@@ -8,6 +8,11 @@
 
 #import "RechangeVC.h"
 #import "RechangeTableViewCell.h"
+#import "WXApi.h"
+#import <AlipaySDK/AlipaySDK.h>
+#import "Order.h"
+#import "DataSigner.h"
+
 
 @interface RechangeVC ()<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate>
 @property (nonatomic, strong) UITableView                   *rechangeTableView;
@@ -169,8 +174,88 @@
     [_rechangeTextField resignFirstResponder];
 }
 
-- (void)rechangeAction:(UIButton*)sender {
+
+- (void)weixinPay {
     
+    PayReq *request = [[PayReq alloc] init];
+    request.partnerId = @"10000100";
+    request.prepayId = @"1101000000140415649af9fc314aa427";
+    request.package = @"Sign=WXPay";
+    request.nonceStr = @"a462b76e7436e98e0ed6e13c64b4fd1c";
+    request.timeStamp = [NSDate timeIntervalSinceReferenceDate];
+    request.sign = @"582282D72DD2B03AD892830965F428CB16E7A256";
+    [WXApi sendReq:request];
+}
+
+- (void)alipay {
+    
+    /*============================================================================*/
+    /*=======================需要填写商户app申请的===================================*/
+    /*============================================================================*/
+    NSString *partner = @"";
+    NSString *seller = @"";
+    NSString *privateKey = @"";
+    /*============================================================================*/
+    /*============================================================================*/
+    /*============================================================================*/
+    
+    //partner和seller获取失败,提示
+    if ([partner length] == 0 ||
+        [seller length] == 0 ||
+        [privateKey length] == 0) {
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示"
+                                                        message:@"缺少partner或者seller或者私钥。"
+                                                       delegate:self
+                                              cancelButtonTitle:@"确定"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    /*
+     *生成订单信息及签名
+     */
+    //将商品信息赋予AlixPayOrder的成员变量
+    Order *order = [[Order alloc] init];
+    order.partner = partner;
+    order.seller = seller;
+//    order.tradeNO = [self generateTradeNO]; //订单ID（由商家自行制定）
+//    order.productName = product.subject; //商品标题
+//    order.productDescription = product.body; //商品描述
+//    order.amount = [NSString stringWithFormat:@"%.2f",product.price]; //商品价格
+//    order.notifyURL =  @"http://www.xxx.com"; //回调URL
+    
+    order.service = @"mobile.securitypay.pay";
+    order.paymentType = @"1";
+    order.inputCharset = @"utf-8";
+    order.itBPay = @"30m";
+    order.showUrl = @"m.alipay.com";
+    
+    //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
+    NSString *appScheme = @"alisdkdemo";
+    
+    //将商品信息拼接成字符串
+    NSString *orderSpec = [order description];
+    NSLog(@"orderSpec = %@",orderSpec);
+    
+    //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
+    id<DataSigner> signer = CreateRSADataSigner(privateKey);
+    NSString *signedString = [signer signString:orderSpec];
+    
+    //将签名成功字符串格式化为订单字符串,请严格按照该格式
+    NSString *orderString = nil;
+    if (signedString != nil) {
+        orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
+                       orderSpec, signedString, @"RSA"];
+        
+        [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
+            NSLog(@"reslut = %@",resultDic);
+        }];
+    }
+}
+
+- (void)rechangeAction:(UIButton*)sender {
 }
 
 - (void)selectButtonAction:(UIButton*)sender event:(UIEvent*)event {
